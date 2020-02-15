03ceba0020da5be267f15915d04744a24cd6e91e
Guliaka <guliaka@gmail.com>
2020-02-15 00:21:53 -0800

    twiaking install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-sudo apt install -y python-gps python-serial libpython-dev python-numpy python-scipy swig python-pil python-flask python-gevent-websocket python-opengl python-pip python3-wxgtk4.0 python3-opengl python-wxgtk3.0 python-opengl
+sudo apt install -y python-gps python-serial libpython-dev python-numpy python-scipy swig python-pil python-flask python-gevent-websocket python-opengl python-pip 
+sudo apt install -y git python3-wxgtk4.0 python3-opengl python-wxgtk3.0 python-opengl wiringpi
 
 sudo pip install setuptools
 sudo pip install ujson
 sudo pip install pyudev
 sudo pip install pyglet
 sudo pip install pywavefront
-sudo pip install flask_serialio
+sudo pip install flask_socketio
 
+sudo apt update
+sudo apt upgrade -y
 
 cd ~
 git clone https://github.com/guliaka/RTIMULib2
@@ -17,14 +20,24 @@ python setup.py build
 sudo python setup.py install
 
 cd ~
-git clone https://github.com/guliaka/pypilot
+git clone -b dev1 https://github.com/guliaka/pypilot
 git clone --depth 1 https://github.com/guliaka/pypilot_data
 cp -rv pypilot_data/* pypilot
 cd pypilot
 python setup.py build
 sudo python setup.py install
 
+cd ~
+sudo cp pypilot/scripts/debian/etc/systemd/system/* /etc/systemd/system/
+
 sudo systemctl enable pypilot
 sudo systemctl enable pypilot_web
+sudo systemctl enable pypilot_hat
+sudo systemctl enable pypilot_boatimu
 sudo systemctl start pypilot
 sudo systemctl start pypilot_web
+
+mkdir .pypilot
+chmod 775 .pypilot
+
+sudo reboot
